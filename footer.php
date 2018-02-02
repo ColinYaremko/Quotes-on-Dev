a3c13a3dd09c96d376243c8c695bef57d764c728
Colin Yaremko <remta@shaw.ca>
2018-02-01 18:17:32 -0800

    hid second footer menu for display

--- footer.php
+++ footer.php
@@ -12,23 +12,23 @@
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="footer-site-info">
 				<div class="menu-main-navigation-container">
-					<ul id="primary-menu" class="footer-navigation">
+			<!--		<ul id="primary-menu" class="footer-navigation">
 						<li id="menu-item-153" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-153"><a href="http://localhost:8080/wpsandbox/about/">About</a></li>
 						<li id="menu-item-154" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-154"><a href="http://localhost:8080/wpsandbox/archives/">Archives</a></li>
 						<li id="menu-item-155" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-155"><a href="http://localhost:8080/wpsandbox/submit/">Submit a Quote</a></li>
-     		  </ul>
+     		  </ul>  -->
 					<div class="wp-footer-nav-menu">
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 						<p>Brought to you by <a href="http://www.redacademy.com/">RED Academy</a></p>
 					</div> <!-- wp-footer-nav-menu -->
 				</div>	<!-- menu-main-navigation-container -->				
-					<p>Brought to you by <a href="http://www.redacademy.com/">RED Academy</a></p>
-				</div><!-- footer-site-info -->
+			<!--		<p>Brought to you by <a href="http://www.redacademy.com/">RED Academy</a></p>  -->
+				</div><!-- footer-site-info -->  
 			</footer><!-- #colophon -->
 		<!-- </div> -->
 		<!-- #page -->
 		</div> <!-- id="page" class="hfeed site -->
-		<?php wp_footer(); ?>
+		<?php wp_footer(); ?>   
 
 	</body>
 </html>
